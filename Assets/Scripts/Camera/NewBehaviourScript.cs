6237da53556d5f98ed231d4270e0f75aa629c7f1
BramG08 <bgrippeling08@gmail.com>
2024-11-04 18:47:42 +0100

    Camera take 2

--- Assets/Scripts/Camera/NewBehaviourScript.cs
+++ Assets/Scripts/Camera/NewBehaviourScript.cs
@@ -15,7 +15,7 @@ public class PlayerCam : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.Locked;  
         Cursor.visible = false; 
-            }
+    }
 
     private void Update()
     {
@@ -27,7 +27,7 @@ public class PlayerCam : MonoBehaviour
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f , 90f);
 
-        transform.rotation = Quaternion.Euler (xRotation, yRotation, 0);
+        transform.rotation = transform.rotation*Quaternion.Euler (xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
     }
 
